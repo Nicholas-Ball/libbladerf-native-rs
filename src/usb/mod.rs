61686188cd340fca390cf4267f7d0bc92c01cea1
Nicholas-Ball <nicholasball085@gmail.com>
2025-01-11 03:44:57 -0500

    Added Enable and Disable modules

--- src/usb/mod.rs
+++ src/usb/mod.rs
@@ -1,16 +1,15 @@
-use std::future::Future;
 use ::nusb::{DeviceInfo, Interface};
 use anyhow::Result;
 
 const BLADE_USB_CMD_QUERY_VERSION: u8 = 0;
-const BLADE_USB_CMD_QUERY_FPGA_STATUS: u8 = 2;
-const BLADE_USB_CMD_BEGIN_PROG: u8 = 3;
-const BLADE_USB_CMD_END_PROG: u8 = 4;
-const BLADE_USB_CMD_RF_RX: u8 = 5;
-const BLADE_USB_CMD_RF_TX: u8 = 6;
-const BLADE_USB_CMD_QUERY_DEVICE_READY: u8 = 7;
-const BLADE_USB_CMD_QUERY_FLASH_ID: u8 = 8;
-const BLADE_USB_CMD_QUERY_FPGA_SOURCE: u8 = 9;
+const BLADE_USB_CMD_QUERY_FPGA_STATUS: u8 = 1;
+const BLADE_USB_CMD_BEGIN_PROG: u8 = 2;
+const BLADE_USB_CMD_END_PROG: u8 = 3;
+const BLADE_USB_CMD_RF_RX: u8 = 4;
+const BLADE_USB_CMD_RF_TX: u8 = 5;
+const BLADE_USB_CMD_QUERY_DEVICE_READY: u8 = 6;
+const BLADE_USB_CMD_QUERY_FLASH_ID: u8 = 7;
+const BLADE_USB_CMD_QUERY_FPGA_SOURCE: u8 = 8;
 const BLADE_USB_CMD_FLASH_READ: u8 = 100;
 
 #[cfg(feature = "nusb")]
@@ -28,40 +27,88 @@ pub struct Device {
 }
 
 #[cfg(feature = "nusb")]
-pub async fn get_version(interface: &mut Interface) -> Result<[u8;4]> {
-    nusb::nusb_bladerf_to_host::<0,0,0,4>(interface).await
-}
-
-#[cfg(feature = "nusb")]
 pub async fn list_devices<const len: usize>() -> Result<[Option<Device>; len]> {
     nusb::list_devices::<len, 0x2CF0>().await
 }
 
+#[cfg(feature = "nusb")]
 impl Device{
-    #[cfg(feature = "nusb")]
     pub fn is_connected(&self) -> bool {
         self.interface.is_some()
     }
     
-    #[cfg(feature = "nusb")]
     pub async fn connect(&mut self) -> Result<()> {
         // Connect to the device
         self.interface = Some(self.device.open()?.claim_interface(0)?);
         
         Ok(())
     }
-
-    #[cfg(feature = "nusb")]
-    pub async fn get_version(&mut self) -> Result<[u8;4]>{
+    
+    pub async fn enable_rx(&mut self) -> Result<()> {
         if let Some(int) = &mut self.interface {
-            nusb::nusb_bladerf_to_host::<BLADE_USB_CMD_QUERY_VERSION,0,0,4>(int).await
-        }else {
+            let test = nusb::nusb_bladerf_to_host::<BLADE_USB_CMD_RF_RX,1,0,4>(int).await?;
+            
+            if test == [0,0,0,0] {
+                Ok(())
+            } else {
+                Err(anyhow::anyhow!("Error enabling RX"))
+            }
+        } else {
+            Err(anyhow::anyhow!("Device not connected"))
+        }
+    }
+
+    pub async fn disable_rx(&mut self) -> Result<()> {
+        if let Some(int) = &mut self.interface {
+            let test = nusb::nusb_bladerf_to_host::<BLADE_USB_CMD_RF_RX,0,0,4>(int).await?;
+
+            if test == [64,0,0,0] {
+                Ok(())
+            } else {
+                Err(anyhow::anyhow!("Error disabling RX"))
+            }
+        } else {
             Err(anyhow::anyhow!("Device not connected"))
         }
     }
 
 
-    #[cfg(feature = "nusb")]
+    pub async fn enable_tx(&mut self) -> Result<()> {
+        if let Some(int) = &mut self.interface {
+            let test = nusb::nusb_bladerf_to_host::<BLADE_USB_CMD_RF_TX,1,0,4>(int).await?;
+
+            if test == [0,0,0,0] {
+                Ok(())
+            } else {
+                Err(anyhow::anyhow!("Error enabling RX"))
+            }
+        } else {
+            Err(anyhow::anyhow!("Device not connected"))
+        }
+    }
+
+    pub async fn disable_tx(&mut self) -> Result<()> {
+        if let Some(int) = &mut self.interface {
+            let test = nusb::nusb_bladerf_to_host::<BLADE_USB_CMD_RF_TX,0,0,4>(int).await?;
+
+            if test == [64,0,0,0] {
+                Ok(())
+            } else {
+                Err(anyhow::anyhow!("Error disabling RX"))
+            }
+        } else {
+            Err(anyhow::anyhow!("Device not connected"))
+        }
+    }
+
+    pub async fn get_version(&mut self) -> Result<[u8;4]>{
+        if let Some(int) = &mut self.interface {
+            nusb::nusb_bladerf_to_host::<BLADE_USB_CMD_QUERY_VERSION,0,0,4>(int).await
+        } else {
+            Err(anyhow::anyhow!("Device not connected"))
+        }
+    }
+
     pub fn disconnect(&mut self) -> Result<()> {
         // Disconnect from the device
         self.interface = None;
--- tests/basic.rs
+++ tests/basic.rs
@@ -1,3 +1,4 @@
+use tokio::time::sleep;
 use libbladerf_native_rs::usb::list_devices;
 
 #[tokio::test]
@@ -14,6 +15,50 @@ async fn connect_test() {
     let version = device.get_version().await.unwrap();
 
     assert_eq!(version, [2, 0, 4, 0]);
+
+    device.disconnect().unwrap();
+
+    assert!(!device.is_connected());
+}
+
+#[tokio::test]
+async fn enable_rx_test() {
+    let mut devices = list_devices::<4>().await.unwrap();
+    let mut device = devices[0].take().unwrap();
+
+    assert!(!device.is_connected());
+
+    device.connect().await.unwrap();
+
+    assert!(device.is_connected());
+
+    device.enable_rx().await.unwrap();
+    
+    sleep(std::time::Duration::from_secs(10)).await;
+    
+    device.disable_rx().await.unwrap();
+
+    device.disconnect().unwrap();
+
+    assert!(!device.is_connected());
+}
+
+#[tokio::test]
+async fn enable_tx_test() {
+    let mut devices = list_devices::<4>().await.unwrap();
+    let mut device = devices[0].take().unwrap();
+
+    assert!(!device.is_connected());
+
+    device.connect().await.unwrap();
+
+    assert!(device.is_connected());
+
+    device.enable_tx().await.unwrap();
+
+    sleep(std::time::Duration::from_secs(10)).await;
+
+    device.disable_tx().await.unwrap();
 
     device.disconnect().unwrap();
 
